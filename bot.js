9370850d73c5ad7534c3e649367946aa90abf0c3
Gabriel <dehan.gabriel@gmail.com>
2018-01-19 22:05:09 +0100

    Added a side app that pings every 15 mn to make sure heroku doesn't sleep

--- bot.js
+++ bot.js
@@ -42,3 +42,34 @@ bot.on('messageReactionAdd', function(messageReaction, user) {
   }
 });
 
+// SIDE WEB-APP USED ONLY TO PREVENT HEROKU FROM SLEEPING
+
+const express = require('express');
+const app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+const port = process.env.PORT || 5000;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the `public` directory for assets (css/js/img)
+app.use(express.static(__dirname + '/public'));
+
+// set the home page route
+app.get('/', (request, response) => {
+  // ejs render automatically looks in the views folder
+  response.render('index');
+});
+
+app.listen(port, () => {
+  // will echo 'Our app is running on http://localhost:5000 when run locally'
+  console.log('Our app is running on http://localhost:' + port);
+});
+
+
+// pings server every 15 minutes to prevent dynos from sleeping
+setInterval(() => {
+  http.get('http://your-app-name.herokuapp.com');
+}, 900000);
